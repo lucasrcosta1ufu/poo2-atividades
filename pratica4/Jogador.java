5c0428dbf38668b0fd528ad24486be64a758201c
Elymas-Magus <pedropaulo.spaiva.a@gmail.com>
2022-01-29 22:12:22 -0300

    ajustes de imagem e nome do robo, nova identacao

--- pratica4/Jogador.java
+++ pratica4/Jogador.java
@@ -1,198 +1,233 @@
 import java.util.Observer;
 import java.util.Observable;
 import java.util.ArrayList;
+import java.awt.image.BufferedImage;
 
 /**
  * Write a description of class Jogador here.
  * 
- * @author (your name) 
+ * @author (your name)
  * @version (a version number or a date)
  */
-public class Jogador extends Observable
-{
+public class Jogador extends Observable {
     private Corre c;
     private Ataca a;
     private Estado vida;
-    private int quantidade;  
+    private int quantidade;
     private Escudo escudo;
     private String nome;
     private int x = 0;
-    private int y = 0; 
-    private int maxX,maxY;   
+    private int y = 0;
+    private int maxX, maxY;
     private int tamX, tamY;
-    
-    public Jogador(int x, int y, int maxX, int maxY){
+    private BufferedImage image;
+
+    public Jogador(int x, int y, int maxX, int maxY, BufferedImage image) {
         this.x = x;
         this.y = y;
         this.maxX = maxX;
         this.maxY = maxY;
         this.tamX = tamX;
         this.tamY = tamY;
-        vida = new EstadoNormal(this);
-        setQuantidade(70);
+        this.image = image;
+
+        this.vida = new EstadoNormal(this);
+        this.setQuantidade(70);
     }
-    
-    public Estado getVida(){
+
+    public Estado getVida() {
         return this.vida;
     }
-    
-    public void setVida(Estado vida){
+
+    public void setVida(Estado vida) {
         this.vida = vida;
     }
-    
-    public int getQuantidade(){
+
+    public int getQuantidade() {
         return this.quantidade;
     }
-    
-    public void setQuantidade(int quantidade){
+
+    public void setQuantidade(int quantidade) {
         this.quantidade = quantidade;
     }
-    
+
     public void recebeAtaque(int ataque) {
         vida.dano(ataque);
     }
-    
-    public void recompensa(){
+
+    public void recompensa() {
         vida.ganhaVida();
     }
-     
-    public Corre getC(){
+
+    public Corre getC() {
         return this.c;
     }
-    
-    public void setC(Corre c){
+
+    public void setC(Corre c) {
         this.c = c;
     }
-    
-    public Ataca getA(){
+
+    public Ataca getA() {
         return this.a;
     }
-    
-    public void setA(Ataca a){
+
+    public void setA(Ataca a) {
         this.a = a;
     }
-    
+
     public void setEscudo(Escudo escudo) {
         this.escudo = escudo;
     }
-    
-    public Escudo getEscudo () {
+
+    public Escudo getEscudo() {
         return this.escudo;
+    }
+
+    public void setAvatar(BufferedImage image) {
+        this.image = image;
+    }
+
+    public BufferedImage getAvatar() {
+        return this.image;
     }
 
     public void addEscudo(Escudo escudo) {
         this.escudo.setNext(escudo);
-    }    
-    
-    public void corrida(){
+    }
+
+    public void corrida() {
         c.correr();
     }
-    
-    public void ataque(){
+
+    public void ataque() {
         a.atacar();
     }
-    
+
     public int getX() {
         return this.x;
     }
 
     public int getY() {
         return this.y;
-    }    
-    
-    public void setX(int x){this.x = x;}
-    public void setY(int y){this.y = y;}
-    
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
     public void setPos(int x, int y) {
         this.x = x;
-        this.y = y;        
+        this.y = y;
     }
-    
-    public void show(){
+
+    public void show() {
         setChanged();
         notifyObservers();
     }
-    
+
     // ------------------------------------------------------- //
-          
-    public synchronized void enviaAtaque(ArrayList<Robo> robos){
-        //se estiver em distancia de ataque     
-        for(int i = 0; i < robos.size(); i++){
-            if ((Math.abs(this.getX() - robos.get(i).getX()) <= 10) && (Math.abs(this.getY() - robos.get(i).getY()) <= 10) ){
-                robos.get(i).getVida().verificaEstado();
-                if (robos.get(i).getVida() instanceof RoboMorto){
-                    this.deleteObserver(robos.get(i));
-                    robos.remove(robos.get(i));
+
+    public synchronized void enviaAtaque(ArrayList<Robo> robos) {
+    	Robo robo;
+        // se estiver em distancia de ataque
+        for (int i = 0; i < robos.size(); i++) {
+        	robo = robos.get(i);
+            if (
+            	(Math.abs(this.getX() - robo.getX()) <= 10) &&
+            	(Math.abs(this.getY() - robo.getY()) <= 10)
+            ) {
+                robo.getVida().verificaEstado();
+                if (robo.getVida() instanceof RoboMorto) {
+                    this.deleteObserver(robo);
+                    robos.remove(robo);
                     continue;
-                }else{
-                    robos.get(i).recebeAtaque(this.getA().atacar());
-                    if (Math.random() < 0.5){
-                        robos.get(i).setPos(robos.get(i).getX()+(int)(100*Math.random()), robos.get(i).getY()-(int)(100*Math.random()));
-                    }
-                    else {
-                        robos.get(i).setPos(robos.get(i).getX()-(int)(100*Math.random()), robos.get(i).getY()+(int)(100*Math.random()));
+                } else {
+                    robo.recebeAtaque(this.getA().atacar());
+                    if (Math.random() < 0.5) {
+                        robo.setPos(
+	                        robo.getX() + (int) (100 * Math.random()),
+	                        robo.getY() - (int) (100 * Math.random())
+                        );
+                    } else {
+                        robo.setPos(
+                            robo.getX() - (int) (100 * Math.random()),
+                            robo.getY() + (int) (100 * Math.random())
+                        );
                     }
                 }
-                
-                
+            } else {
+                if (robo.getX() > this.x) {
+                    this.x = this.x + (int) (5 * Math.random());
+                } else {
+                    this.x = this.x - (int) (5 * Math.random());
+                }
+
+                if (robo.getY() > this.y) {
+                    this.y = this.y + (int) (5 * Math.random());
+                }                 else {
+                    this.y = this.y - (int) (5 * Math.random());
+                }
+
             }
-            //se precisar andar em direcao a bola
-            else {
-               //se precisa andar para frente 
-               if (robos.get(i).getX() > this.x) this.x = this.x+(int)(5*Math.random());
-               else this.x = this.x-(int)(5*Math.random());
-               //se precisa andar para tras
-               if (robos.get(i).getY() > this.y) this.y = this.y+(int)(5*Math.random());
-               else this.y = this.y-(int)(5*Math.random());
-                       
+
+            if (robo.getX() < 0) {
+                robo.setX(100);
             }
-            
-            if (robos.get(i).getX() < 0) robos.get(i).setX(100);
-            if (robos.get(i).getY() < 0) robos.get(i).setY(100);
+            if (robo.getY() < 0) {
+                robo.setY(100);
+            }
         }
     }
-       
+
     public void coletaEscudo(ArrayList<Escudo> escudos) {
-        for(int i = 0; i <= escudos.size() ; i ++){
-            if ((Math.abs(this.getX() - escudos.get(i). getX()) <= 2) && (Math.abs(this.getY() - escudos.get(i). getY()) <= 2)){
+        for (int i = 0; i <= escudos.size(); i++) {
+            if (
+            	(Math.abs(this.getX() - escudos.get(i).getX()) <= 2) &&
+            	(Math.abs(this.getY() - escudos.get(i).getY()) <= 2)
+            ) {
                 this.addEscudo(escudos.get(i));
             }
         }
     }
-    
-    public String toString(){
+
+    public String toString() {
         return this.nome;
-    }    
-    
-    
+    }
+
     public void moveToUp() {
-        if(this.y - this.getC().correr() >= 0)
-            this.setY(this.getY()-this.getC().correr());
-        else
+        if (this.y - this.getC().correr() >= 0) {
+            this.setY(this.getY() - this.getC().correr());
+        } else {
             this.y = 0;
+        }
     }
-    
+
     public void moveToDown() {
-        if(this.y + this.getC().correr() <= this.maxY)
-            this.setY(this.getY()+this.getC().correr());  
-        else
+        if (this.y + this.getC().correr() <= this.maxY) {
+            this.setY(this.getY() + this.getC().correr());
+        } else {
             this.y = this.maxY;
+        }
     }
-    
+
     public void moveToLeft() {
-        if(this.x - this.getC().correr() >= 0)
-            this.setX(this.getX()-this.getC().correr());
-        else
+        if (this.x - this.getC().correr() >= 0) {
+            this.setX(this.getX() - this.getC().correr());
+        } else {
             this.x = 0;
+        }
     }
-    
+
     public void moveToRight() {
-        if(this.x + this.getC().correr() <= this.maxX)
-            this.setX(this.getX()+this.getC().correr());
-        else
+        if (this.x + this.getC().correr() <= this.maxX) {
+            this.setX(this.getX() + this.getC().correr());
+        } else {
             this.x = this.maxX;
+        }
     }
-    
-    
-    
+
 }
